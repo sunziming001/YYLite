75c67ce14028bb6c80ee50ce5ddfb6eec9ea0e49
sunziming <sunziming001@126.com>
2026-03-19 22:04:22 +0800

    first window

---
+++ projects/YYLite/src/FrameWindowWnd.cpp
@@ -0,0 +1,50 @@
+#include "FrameWindowWnd.h"
+
+
+
+CFrameWindowWnd::CFrameWindowWnd()
+{
+}
+
+LPCTSTR CFrameWindowWnd::GetWindowClassName() const
+{
+	return _T("UIMainFrame");
+}
+
+UINT CFrameWindowWnd::GetClassStyle() const
+{
+	return UI_CLASSSTYLE_FRAME | CS_DBLCLKS;
+}
+
+void CFrameWindowWnd::OnFinalMessage(HWND)
+{
+	delete this;
+}
+
+void CFrameWindowWnd::Notify(TNotifyUI& msg)
+{
+	if (msg.sType == _T("click")) {
+		if (msg.pSender->GetName() == _T("closebtn")) {
+			Close();
+		}
+	}
+}
+
+LRESULT CFrameWindowWnd::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (uMsg == WM_CREATE) {
+		m_pm.Init(m_hWnd);
+		CControlUI* pButton = new CButtonUI;
+		pButton->SetName(_T("closebtn"));
+		pButton->SetBkColor(0xFFFF0000);
+		m_pm.AttachDialog(pButton);
+		m_pm.AddNotifier(this);
+		return 0;
+	}
+	else if (uMsg == WM_DESTROY) {
+		::PostQuitMessage(0);
+	}
+	LRESULT lRes = 0;
+	if (m_pm.MessageHandler(uMsg, wParam, lParam, lRes)) return lRes;
+	return CWindowWnd::HandleMessage(uMsg, wParam, lParam);
+}
--- projects/YYLite/src/main.cpp
+++ projects/YYLite/src/main.cpp
@@ -1,9 +1,19 @@
-#include <iostream>
+#include <windows.h>
+#include "FrameWindowWnd.h"
 
 
-int main(int argc, char** argv)
+// 程序入口及 Duilib 初始化部分
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR
+	/*lpCmdLine*/, int nCmdShow)
 {
+	CPaintManagerUI::SetInstance(hInstance);
+	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath());
+	CFrameWindowWnd* pFrame = new CFrameWindowWnd();
+	if (pFrame == NULL) return 0;
+	pFrame->Create(NULL, _T("测试"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	pFrame->ShowWindow(true);
+	CPaintManagerUI::MessageLoop();
 
-	std::cout << "111" << std::endl;
 	return 0;
+
 }
